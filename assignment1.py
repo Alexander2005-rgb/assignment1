0d0de0229226e751a517c2e6f06446d9d2d676d3
Alexander2005-rgb <shivendra16062005@gmail.com>
2025-03-23 15:43:25 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,20 @@
+#basic operation on these two numbers
+a= int(input("Enter a first number: "))
+b =int(input("Enter a second number: "))
+c=a+b
+print("Addition :",c)
+c=a-b
+print("Subtraction :",c)
+c=a*b
+
+print("Multiplication :",c)
+c=a/b
+print("Division :",c)
+
+
+#create a personalise greeting
+
+a= input("Enter your First  name: ")
+b= input ("Enter your Last name: ")
+c=a+b
+print(f"Hello ! {c}, Welcome to the python program!")
